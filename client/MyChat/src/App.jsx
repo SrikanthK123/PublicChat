0b1d2ccb398ca5dec19df0fc3347e94225e423ff
SrikanthK123 <93971062+SrikanthK123@users.noreply.github.com>
2024-07-15 12:11:51 +0530

    AllChanges8

--- client/MyChat/src/App.jsx
+++ client/MyChat/src/App.jsx
@@ -1,22 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Chat from './components/Chat';
 
 function App() {
-  
-
   return (
-    <>
-     <Router>
-            <Routes>
-                <Route path='/PublicChat' element={<Login/>}> </Route>
-                <Route path='/chat' element={<Chat/>}> </Route>
-            </Routes>
-            </Router>   
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path='/PublicChat/Login' element={<Login />} />
+        <Route path='/PublicChat/chat' element={<Chat />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
