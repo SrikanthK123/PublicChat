c8603351bbece3021e6e99a3d5d1ef5582b12e72
SrikanthK123 <93971062+SrikanthK123@users.noreply.github.com>
2024-07-15 11:59:45 +0530

    AllChanges7

--- client/MyChat/vite.config.js
+++ client/MyChat/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Base path for GitHub Pages
+  base: '/Login',  // Base path for GitHub Pages
   build: {
     outDir: 'dist',  // Output directory for the build files
   },
