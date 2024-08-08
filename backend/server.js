3e88e0b732b4993f04842e24e62964c619e3b4f1
SrikanthK123 <93971062+SrikanthK123@users.noreply.github.com>
2024-08-08 19:44:37 +0530

    ChangeFew529

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.get('/api/config', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
